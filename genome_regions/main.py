45ca9f1f07ffe3fb6c26415c6e302b5a50446b07
Philippa Muston <pm22@sanger.ac.uk>
2024-04-22 14:55:01 +0100

    Working on cmd args

--- genome_regions/main.py
+++ genome_regions/main.py
@@ -1,27 +1,28 @@
 # Description: This is the main file for the genome_regions package. It is used to validate the file path.
 from pathlib import Path
 from utils import get_regions_from_file, create_rows_from_regions, create_segments_from_regions, generate_rows_file, generate_segments_file
+import argparse, sys
+
+
 
 
 def main():
     """"""
-    # print('Please enter the path:')
-    # input_file = Path(input())
-    input_file = Path('../Regions_Small.txt')
+    #'../Regions_Large.txt'
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-i', '--input',  help='image file',  required=True)
+    parser.add_argument('-o', '--output',  help='output path')
+    args=parser.parse_args()
+    input_file = Path(args.i)
+    output_path = Path(args.o)
+    print(input_file)
+    print(output_path)
+    input_file = Path('../Regions_Large.txt')
     output_path = Path('../')
     if not output_path.exists():
         output_path.mkdir()
     if input_file.exists():
         regions = get_regions_from_file(input_file)
-        # regions_dict = create_dictionary_from_regions(regions, True)
-        # total_rows = 0
-        # total_regions = 0
-        # for key in regions_dict.keys():
-        #     total_rows += 1
-        #     for region in regions_dict[key]:
-        #         total_regions += 1
-        #         print(f"{region.Start}\t{region.End}")
-        # print(f"Total rows: {total_rows} Total regions: {total_regions}")
         rows_dict = create_rows_from_regions(regions)
         generate_rows_file(rows_dict, output_path , 'rows_output.txt')
         segments_list = create_segments_from_regions(regions)
